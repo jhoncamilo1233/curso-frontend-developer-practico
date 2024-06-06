433bdf933294a50ed6268e27d28cbca9b935305d
pesadilla666 <cm4704052@gmail.com>
2024-06-06 17:22:00 -0500

    proyecto casi terminado

---
+++ main.js
@@ -0,0 +1,136 @@
+// aca estamos creado  dos varibales una  una donde le vamos a dar el evento y la otra lo que
+//vamos a mostrar
+const emailMenu = document.querySelector(".navbar-email"); // aca ponemos la clase a lo que vamos que quiere que haga una funcion o evento
+const desktopMenu = document.querySelector(".desktop-menu"); // esto es la que queremos salir y transmitir
+
+const menuHomeIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const menuCarritoIco = document.querySelector(".navbar-shopping-cart");
+const productoAside = document.querySelector(".product-detail");
+
+const cardsConatiner = document.querySelector('.cards-container')
+
+//aca llamamos nuestro evento click y darle la funcionalidad toggle
+emailMenu.addEventListener("click", toggleDesktopMenu);
+
+menuHomeIcon.addEventListener("click", toggleMobileMenu);
+
+menuCarritoIco.addEventListener("click", toggleCarritoIco);
+
+//creamos una funcion para poder que este inactivo o no ejemplo ocultar o abrir
+function toggleDesktopMenu() {
+  const isAsideClosed = productoAside.classList.contains("inactive");
+
+  if (!isAsideClosed) {
+    productoAside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+
+  desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  const isAsideClosed = productoAside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    productoAside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoIco() {
+  const isMobileClosed = mobileMenu.classList.contains("inactive");
+  if (!isMobileClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+  productoAside.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+    nombre: 'bike',
+    precio : 120,
+    imagen : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    nombre: 'tabla',
+    precio : 150,
+    imagen : 'https://www.labskateboarding.com/wp-content/uploads/2021/06/DARK-SKULL-WEB-LQ-SOLO-DECK.jpg'
+});
+productList.push({
+    nombre: 'bike',
+    precio : 120,
+    imagen : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    nombre: 'bike',
+    precio : 120,
+    imagen : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    nombre: 'bike',
+    precio : 120,
+    imagen : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    nombre: 'tabla',
+    precio : 150,
+    imagen : 'https://www.labskateboarding.com/wp-content/uploads/2021/06/DARK-SKULL-WEB-LQ-SOLO-DECK.jpg'
+});
+productList.push({
+    nombre: 'bike',
+    precio : 120,
+    imagen : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    nombre: 'bike',
+    precio : 120,
+    imagen : 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+function productos(arr){
+    for( const product of arr){
+        //creamos el div de nuestro producto de la cards 
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+    
+        //vamos a crear la img 
+        const productImg = document.createElement('img');
+        // llamamos product = {name .price, imegen} -> product.imagen
+        productImg.setAttribute('src',product.imagen);
+    
+        //creamos la infromacion del producto
+        const productInfo = document.createElement ('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+        
+        
+        const productPrecio = document.createElement('p');
+        productPrecio.innerText = '$' + product.precio;
+        
+        const productName = document.createElement('p');
+        productName.innerText = product.nombre;
+    
+        productInfoDiv.appendChild(productPrecio);
+        productInfoDiv.appendChild(productName);
+        
+        const productFigure = document.createElement('figure');
+    
+        const productFigureImg = document.createElement('img');
+        productFigureImg.setAttribute('src','./icons/bt_add_to_cart.svg')
+    
+    
+        productFigure.appendChild(productFigureImg);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productFigure);
+        
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        
+        cardsConatiner.appendChild(productCard)
+    
+    }
+}
+productos(productList);
